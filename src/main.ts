e22f71f91ee1bae341b8b7d67634032271a1d710
LuisNav6 <113213452+LuisNav6@users.noreply.github.com>
2024-06-17 20:47:22 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,7 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
