9c2cb233ca4a8bcfc9ce05a8cbea94b9c1b05315
LuisNav6 <113213452+LuisNav6@users.noreply.github.com>
2024-06-17 21:29:38 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Configura CORS para permitir cualquier origen
   app.enableCors({
-    origin: 'http://localhost:4200', // Permite solicitudes desde este origen
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
